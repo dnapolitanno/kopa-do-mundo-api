ed409ea98ac397e6855a7da48b913125792851b6
dnapolitanno <dudadnapolitano@gmail.com>
2023-06-05 13:25:57 -0300

    feat: tarefa 3 finalizada

--- teams/views.py
+++ teams/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView, Request, Response, status
 from .models import Team
 from django.forms.models import model_to_dict
+from utils import data_processing
+from exceptions import *
 
 
 class KopaView(APIView):
@@ -15,6 +17,12 @@ class KopaView(APIView):
         return Response(team_list, status.HTTP_200_OK)
 
     def post(self, request: Request) -> Response:
+
+        try:
+            data_processing(request.data)
+        except (NegativeTitlesError, InvalidYearCupError, ImpossibleTitlesError) as error:
+            return Response({"error": error.message})
+
         team = Team.objects.create(**request.data)
         team_dict = model_to_dict(team)
 
--- utils.py
+++ utils.py
@@ -5,16 +5,17 @@ import datetime
 
 
 def data_processing(dic):
-    titles = dic.get("titles")
-    first_cup = dic.get("first_cup")
+    titles = int(dic.get("titles"))
+    first_cup_date = dic.get("first_cup")
+    first_cup = datetime.datetime.strptime(first_cup_date, "%Y-%m-%d").year
 
     if titles < 0:
-        raise NegativeTitlesError
+        raise NegativeTitlesError("titles cannot be negative")
 
     current_year = datetime.datetime.now().year
     if first_cup < 1930 or (first_cup - 1930) % 4 != 0 or first_cup > current_year:
-        raise InvalidYearCupError
+        raise InvalidYearCupError("there was no world cup this year")
 
     cups_played = (current_year - first_cup) // 4 + 1
     if titles > cups_played:
-        raise ImpossibleTitlesError
+        raise ImpossibleTitlesError("impossible to have more titles than disputed cups")
